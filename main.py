d5b6fd00f10940ad799ae990cdca40f326181cf7
Tomas Bert <tomas.bert@almundo.com>
2019-12-04 23:05:44 -0300

    feature: siap processor

--- main.py
+++ main.py
@@ -1,9 +1,40 @@
-from flask import Flask, request, jsonify
+from flask import Flask, flash, Response, request, jsonify, redirect, url_for
+from werkzeug.utils import secure_filename
+from io import StringIO
+import siap_import
+
+ALLOWED_EXTENSIONS = {'csv'}
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "<h1>SIAP</h1>"
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files.get('file')
+        if not file or file.filename == '' or not allowed_file(file.filename):
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            result = siap_import.process_raw(file)
+            name = file.filename.rsplit('.', 1)[0].replace(' ', '').replace('"', '')
+            final_name = f"sirap-{name}.txt"
+            headers = {"Content-disposition": f"attachment; filename={final_name}"}
+            return Response(result, mimetype="text/plain", headers=headers)
+    return '''
+    <!doctype html>
+    <title>SIAP Import</title>
+    <h1>Upload AFIP export</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
---
+++ siap_import.py
@@ -0,0 +1,92 @@
+import pandas as pd
+
+LINE_BREAK = "/r/n"
+
+"""
+"Fecha",
+"Tipo",
+"Punto de Venta",
+"Número Desde",
+"Número Hasta",
+"Cód. Autorización",
+"Tipo Doc. Emisor",
+"Nro. Doc. Emisor",
+"Denominación Emisor",
+"Tipo Cambio",
+"Moneda",
+"Imp. Neto Gravado",
+"Imp. Neto No Gravado",
+"Imp. Op. Exentas",
+"IVA",
+"Imp. Total"
+"""
+
+ZFILLS = [8, 3, 5, 20, 16, 2, 20, 30] + [15] * 8 + [3, 10, 1, 1, 15, 15, 11, 30, 15]
+
+def read_csv(source):
+    df = pd.read_csv(source)
+    return df[pd.notnull(df['IVA']) & df["IVA"] > 0]
+
+def convert_date(date):
+    day, month, year = date.split('/')
+    return f"{year}{month}{day}"
+
+def voucher_type(voucher_desc):
+    return voucher_desc.split(' ')[0]
+
+def currency(curr):
+    return "USD" if curr == "USD" else "PES"
+
+def convert_row(row):
+    cells = [
+        convert_date(row['Fecha']), 
+        voucher_type(row['Tipo']),
+        row['Punto de Venta'],
+        row['Número Desde'],
+        "0",
+        "8",
+        row['Nro. Doc. Emisor'],
+        row['Denominación Emisor'][:30],
+        int(row['Imp. Total'] * 100),
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        int(row['Imp. Neto No Gravado'] * 100),
+        currency(row['Moneda']),
+        f"{float(row['Tipo Cambio']):.6f}",
+        "1",
+        "0",
+        int(row["IVA"] * 100),
+        "0",
+        "0",
+        "0",
+        "0",
+    ]
+    cells_fill = [str(cell).zfill(zeros) for cell, zeros in zip(cells, ZFILLS)]
+    return "".join(cells_fill)
+
+def convert_dataframe(df):
+    return [convert_row(row) for _, row in df.iterrows()]
+
+def save_txt(lines):
+    with open('example/example-siap.txt', 'w') as f:
+        for line in lines:
+            f.write(f"{line}\r\n")
+
+def process_raw(source):
+    df = read_csv(source)
+    lines = convert_dataframe(df)
+    return "\r\n".join(lines)
+
+if __name__ == '__main__':
+    try:
+        df = read_csv('example/example.csv')
+        lines = convert_dataframe(df)
+        save_txt(lines)
+        exit(0)
+    except Exception as e:
+        print(e)
+        exit(1)
