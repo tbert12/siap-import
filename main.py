f20795ebb568694a217a93a0d54ad5e5df8abb21
Tomas Bert <tomas.bert@almundo.com>
2019-12-06 14:41:27 -0300

    feature: add aliquot

--- main.py
+++ main.py
@@ -9,30 +9,51 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 app = Flask(__name__)
 
+def process(request, processor):
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect('/')
+    file = request.files.get('file')
+    if not file or file.filename == '' or not allowed_file(file.filename):
+        flash('No selected file')
+        return redirect('/')
+    if file and allowed_file(file.filename):
+        result, filename = processor(file)
+        headers = {"Content-disposition": f"attachment; filename={filename}"}
+        return Response(result, mimetype="text/plain", headers=headers)
+
+@app.route('/voucher', methods=["POST"])
+def voucer():
+    return process(request, siap_import.process_voucher_raw)
+
+@app.route('/aliquot', methods=["POST"])
+def aliquot():
+    return process(request, siap_import.process_aliquot_raw)
+
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files.get('file')
-        if not file or file.filename == '' or not allowed_file(file.filename):
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            result = siap_import.process_raw(file)
-            name = file.filename.rsplit('.', 1)[0].replace(' ', '').replace('"', '')
-            final_name = f"sirap-{name}.txt"
-            headers = {"Content-disposition": f"attachment; filename={final_name}"}
-            return Response(result, mimetype="text/plain", headers=headers)
     return '''
     <!doctype html>
     <title>SIAP Import</title>
     <h1>Upload AFIP export</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+    <form id="siap" method=post enctype=multipart/form-data>
+      <input type=file name=file><br/>
+      <input id=download type=button value="Descargar para SIAP">
+      <script>
+            var $download = $('#download');
+            var $form = $('#siap');
+            function download(endpoint) {
+                $form.attr('action', endpoint).submit();
+            }
+            $download.on('click', function() {
+                download('/voucher');
+                setTimeout(function() {
+                    download('/aliquot');
+                }, 500);
+            });
+      </script>
     </form>
     '''
 
--- siap_import.py
+++ siap_import.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 LINE_BREAK = "/r/n"
 
-"""
+""" Comprobante
 "Fecha",
 "Tipo",
 "Punto de Venta",
@@ -21,7 +21,8 @@ LINE_BREAK = "/r/n"
 "Imp. Total"
 """
 
-ZFILLS = [8, 3, 5, 20, 16, 2, 20, 30] + [15] * 8 + [3, 10, 1, 1, 15, 15, 11, 30, 15]
+VOUCHER_ZFILLS = [8, 3, 5, 20, 16, 2, 20, 30] + [15] * 8 + [3, 10, 1, 1, 15, 15, 11, 30, 15]
+ALIQUOT_ZFILLS = [3, 5, 20, 2, 20, 15, 4, 15]
 
 def read_csv(source):
     df = pd.read_csv(source)
@@ -37,7 +38,7 @@ def voucher_type(voucher_desc):
 def currency(curr):
     return "USD" if curr == "USD" else "PES"
 
-def convert_row(row):
+def convert_voucher(row):
     row_voucher_type = voucher_type(row['Tipo'])
     total = -float(row['Imp. Total']) if row_voucher_type == "3" else float(row['Imp. Total'])
     cells = [
@@ -49,41 +50,72 @@ def convert_row(row):
         "80".ljust(2),
         row['Nro. Doc. Emisor'],
         row['Denominación Emisor'][:30].ljust(30),
-        int(total * 100),
+        int(total * 100 + .5),
         0,
         0,
         0,
         0,
         0,
         0,
-        int(row['Imp. Neto No Gravado'] * 100),
+        int(row['Imp. Neto No Gravado'] * 100 + .5),
         currency(row['Moneda']),
         f"{float(row['Tipo Cambio']):.6f}".replace('.', ''),
         "1",
         "0",
-        int(row["IVA"] * 100),
+        int(row["IVA"] * 100 + .5),
         "0",
         "0",
         " " * 30,
         "0",
     ]
-    cells_fill = [str(cell).zfill(zeros) for cell, zeros in zip(cells, ZFILLS)]
+    cells_fill = [str(cell).zfill(zeros) for cell, zeros in zip(cells, VOUCHER_ZFILLS)]
     return "".join(cells_fill)
 
-def convert_dataframe(df):
-    return [convert_row(row) for _, row in df.iterrows()]
+def convert_aliquot(row):
+    row_voucher_type = voucher_type(row['Tipo'])
+    total = -float(row['Imp. Total']) if row_voucher_type == "3" else float(row['Imp. Total'])
+    cells = [
+        row_voucher_type,
+        row['Punto de Venta'],
+        row['Número Desde'],
+        "80".ljust(2),
+        row['Nro. Doc. Emisor'],
+        int(row["Imp. Neto Gravado"] * 100 + .5),
+        "5",
+        int(row["IVA"] * 100 + .5),
+    ]
+    cells_fill = [str(cell).zfill(zeros) for cell, zeros in zip(cells, ALIQUOT_ZFILLS)]
+    return "".join(cells_fill)
+
+def convert_dataframe_voucher(df):
+    return [convert_voucher(row) for _, row in df.iterrows()]
+
+def convert_datafame_aliquot(df):
+    return [convert_aliquot(row) for _, row in df.iterrows()]
+
+def save_voucher_txt(filename, lines):
+    filename_out = f"{filename.split('.', 1)[0]}-comprobante.txt"
+    save_txt(filename_out, lines)
+
+def save_aliquot_txt(filename, lines):
+    filename_out = f"{filename.split('.', 1)[0]}-alicuota.txt"
+    save_txt(filename_out, lines)
 
 def save_txt(filename, lines):
-    filename_out = f"{filename.split('.', 1)[0]}-out.txt"
-    print(f"Saving in '{filename_out}'")
-    with open(filename_out, 'w') as f:
+    print(f"Saving in '{filename}'")
+    with open(filename, 'w') as f:
         for line in lines:
             f.write(f"{line}\r\n")
 
-def process_raw(source):
+def process_voucher_raw(source):
     df = read_csv(source)
-    lines = convert_dataframe(df)
-    return "\r\n".join(lines)
+    lines = convert_dataframe_voucher(df)
+    return "\r\n".join(lines), f"{source.filename.rsplit('.', 1)[0]}-comprobante.txt"
+
+def process_aliquot_raw(source):
+    df = read_csv(source)
+    lines = convert_datafame_aliquot(df)
+    return "\r\n".join(lines), f"{source.filename.rsplit('.', 1)[0]}-alicuota.txt"
 
 if __name__ == '__main__':
     try:
@@ -91,8 +123,10 @@ if __name__ == '__main__':
         filename = sys.argv[1]
         print(f"Converting '{filename}'")
         df = read_csv(filename)
-        lines = convert_dataframe(df)
-        save_txt(filename, lines)
+        voucher_lines = convert_dataframe_voucher(df)
+        save_voucher_txt(filename, voucher_lines)
+        aliquot_lines = convert_datafame_aliquot(df)
+        save_aliquot_txt(filename, aliquot_lines)
         exit(0)
     except Exception as e:
         print(e)
